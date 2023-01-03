a290b81e13ccd68cab7e7d4bfbacb1f45c46e33b
paulosradavelli <paulosradavelli@gmail.com>
2023-01-03 17:50:00 -0300

    Add loop and inserts user input

--- odin-rps/index.js
+++ odin-rps/index.js
@@ -1,3 +1,7 @@
+
+let computerScore = 0;
+let playerScore = 0;
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -10,33 +14,45 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase()
     if(playerSelection == "rock" && computerSelection == "paper"){
+        computerScore++
         return "You lose! Paper beats Rock"
     }
     if(playerSelection == "paper" && computerSelection == "scissors"){
+        computerScore++
         return "You lose! Scissor beats Paper"
     }
     if(playerSelection == "scissors" && computerSelection == "rock"){
+        computerScore++
         return "You lose! Paper beats scissors"
     }
 
     if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore++
         return "You win! Rock beats scissors"
     }
     if(playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore++
         return "You win! Scissor beats paper"
     }
     if(playerSelection == "paper" && computerSelection == "rock"){
+        playerScore++
         return "You win! Paper beats rock"
     }
 
     if(playerSelection == computerSelection){
+        playerScore++
+        computerScore++
         return "It's a Draw!"
     }
 }
 
-const playerSelection = "rock";
 function game(){
-    for(let i = 0 ; i < 5 ; i++){
-        console.log(playRound(playerSelection, getComputerChoice()))
+    for(let i = 0 ; i <= 10 ; i++){
+        if(computerScore == 5 || playerScore == 5){
+            console.log(`Your score: ${playerScore} | Computer score: ${computerScore}`);
+            return;
+        }
+        console.log(playRound(prompt(), getComputerChoice()))
     }
 }
+
