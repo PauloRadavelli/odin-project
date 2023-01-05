adfc8aa3ef62e00c37f4c01c02ff4c1a3f5572b9
paulosradavelli <paulosradavelli@gmail.com>
2023-01-05 11:59:42 -0300

    finish game

--- odin-rps/index.js
+++ odin-rps/index.js
@@ -1,9 +1,12 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
-const userDisplay = document.querySelector("#user-score");
-const computerDisplay = document.querySelector("#computer-score")
-
+const userDisplayScore = document.querySelector("#user-score");
+const computerDisplayScore = document.querySelector("#computer-score")
+const userDisplayChoice = document.querySelector("#user-choice")
+const computerDisplayChoice = document.querySelector("#computer-choice")
+const modal = document.querySelector("#myModal");
+const modalContent = document.querySelector(".modal-content")
 const options = ["rock","paper","scissors"];
 let computerScore = 0;
 let userScore = 0;
@@ -18,41 +21,90 @@ function getComputerChoice(){
 }
 
 function onRockClick() {
-    userChoice = "rock";
     let computerChoice = getComputerChoice();
+    setComputerIcon(computerChoice);
+    userDisplayChoice.src = "./images/rock.png";    
+    userChoice = "rock";
     if(computerChoice == "paper"){
         computerScore++;
     }
     if(computerChoice == "scissors"){
         userScore++;
     }
-    userDisplay.textContent = "Your score: "+userScore;
-    computerDisplay.textContent = "Computer score: "+computerScore
-
+    userDisplayScore.textContent = "Your score: "+userScore;
+    computerDisplayScore.textContent = "Computer score: "+computerScore
+    replay(userScore,computerScore);
 }
 
 function onPaperClick() {
-    userChoice = "paper";
     let computerChoice = getComputerChoice();
+    userDisplayChoice.src = "./images/paper.png";
+    setComputerIcon(computerChoice);
+    userChoice = "paper";
     if(computerChoice == "scissors"){
         computerScore++;
     }
     if(computerChoice == "rock"){
         userScore++;
     }
-    userDisplay.textContent = "Your score: "+userScore;
-    computerDisplay.textContent = "Computer score: "+computerScore
+    userDisplayScore.textContent = "Your score: "+userScore;
+    computerDisplayScore.textContent = "Computer score: "+computerScore
+    replay(userScore,computerScore);
+
 }
 
 function onScissorsClick(){
-    userChoice = "scissors";
     let computerChoice = getComputerChoice();
+    userDisplayChoice.src = "./images/scissors.png";
+    setComputerIcon(computerChoice);
+    userChoice = "scissors";
     if(computerChoice == "rock"){
         computerScore++
     }
     if(computerChoice == "paper"){
         userScore++
     }
-    userDisplay.textContent = "Your score: "+userScore;
-    computerDisplay.textContent = "Computer score: "+computerScore
+    userDisplayScore.textContent = "Your score: "+userScore;
+    computerDisplayScore.textContent = "Computer score: "+computerScore
+    replay(userScore,computerScore);
+}
+
+function setComputerIcon(computerChoice){
+    switch(computerChoice){
+        case "scissors":
+            computerDisplayChoice.src= "./images/scissors.png"
+            break
+        case "rock":
+            computerDisplayChoice.src= "./images/rock.png"
+            break
+        case "paper":
+            computerDisplayChoice.src= "./images/paper.png"
+            break;
+    }
+}
+
+function replay(playerPoints, computerPoints){
+    if(playerPoints == 5){
+        modal.style.display = "block";
+        const victoryText = document.createElement("p");
+        victoryText.textContent = "Congratulations you won!"
+        modalContent.appendChild(victoryText);
+        userScore = 0;
+        computerScore = 0;
+    }
+    if(computerPoints == 5){
+        modal.style.display = "block";
+        modal.style.display = "block";
+        const defeatText = document.createElement("p");
+        defeatText.textContent = "I'm sorry, you lost!"
+        modalContent.appendChild(defeatText);
+        userScore = 0;
+        computerScore = 0;
+    }
+}
+
+function closeModal(){
+    modal.style.display = "none";
+    location.reload()
+
 }
